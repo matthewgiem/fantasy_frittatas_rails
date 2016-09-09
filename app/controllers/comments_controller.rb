e7d8ae36883a8271415621678cfab34bfcc83776
Matthew <matthew.giem@gmail.com>
2016-09-09 10:18:16 -0700

    create CRUD functionality to for the user model

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,48 @@
+class CommentssController < ApplicationController
+
+  def new
+    @user = User.find(params[:user_id])
+    @comment = @user.comments.new
+  end
+
+  def index
+    @comments = Comment.all
+    render :index
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @comment = @user.comments.new(comment_params)
+    if @comment.save
+      redirect_to user_path(@comment.user)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:user_id])
+    @comment = Comment.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+      redirect_to user_path(@comment.user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to user_path(@comment.user)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
